b543d438134907528ae7f647faedcebf08a2533b
Hisamuddin Shaikh <hisamuddin94@gmail.com>
2022-09-28 20:47:42 +0530

    Added Business ratings component in Home page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Description from '../Components/Home/Description'
+import BussinessRatings from '../Components/Home/BussinessRatings'
 
 const Home = () => {
   return (
     <div>
       <Description/>
+      <BussinessRatings/>
     </div>
   )
 }
