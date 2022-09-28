06e354f12898f42d7833257edb3135504f248ea5
ellite07 <shelketushar100@gmail.com>
2022-09-29 03:53:50 +0530

    price and resourse(useCase) page

---
+++ frontend/src/Components/Review/Reviews.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Box, Heading, Text, Grid, GridItem } from "@chakra-ui/react";
+import styles from "./Review.module.css";
+import data from "./data.json";
+
+const Reviews = () => {
+  return (
+    <Box
+    alignContent='center'
+    // mb="50px"
+    height={{
+        base: '100%', // 0-48em
+        md: '50%', // 48em-80em,
+        xl: '25%', // 80em+
+      }}
+      // bg='green'
+      width={[
+        '100%', // 0-30em
+      ]}>
+ <Heading
+        w="100%"
+        as='h1'
+        fontsize={{ base: '24px', md: '40px', lg: '56px' }}
+        noOfLines={1}
+        color="#375d75"
+        letterSpacing="1px"
+        mt={['5px', '15px', '18px']}
+        align="center"
+        // bg='blue'
+        
+        p={['3px 8px' , '5px 18px']} 
+
+        >
+        My Hours has changed the way our customers work
+        </Heading>
+
+        <Text
+        letterSpacing="1px"
+        fontSize="lg"
+        align="center"
+        p={['12px', '4px', '6px', '8px']}
+        >
+        We’ve helped 100.000+ people track their work since 2002. Our software does not include employee monitoring and never will.
+        </Text>
+
+        <Grid
+        w="100%"
+        // bg='blue'
+        p={['0px 8px' , '0px 18px']} 
+        templateColumns={[ "repeat(2, 1fr)" , "repeat(3, 2fr)" ]}
+        templateRows="atuo"
+        gap={3}
+        mt="30px">
+           
+           {data.map((el, ind) => (
+             <GridItem rounded="20" p="20" 
+            >
+             <Text
+               letterSpacing="0.5px"
+               fontWeight="350"
+               fontSize={[ '15px',  '4px', '16px' ]}
+               textAlign="center"
+             >
+
+               {el.review1}
+               <span className={styles.highlighter}>{el.review2}</span>{" "}
+             </Text>
+             <Heading
+               fontWeight="700"
+               fontStyle="italic"
+               size="md"
+               mt="3"
+               textAlign="left"
+             >
+                {el.author}
+             </Heading>
+           </GridItem>
+              ))}
+        
+        </Grid>
+
+
+        <h1>Footer Component</h1>
+    </Box>
+  )
+}
+
+export default Reviews
--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Pages/Home'
 import Login from '../Pages/Login'
+import Pricing from '../Pages/Pricepage/Pricing'
+import Resources from '../Pages/Resources/Resources'
 
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<Home/>}/>
         {/* <Route path='/how-it-works' element={<Product/>}/> */}
-        {/* <Route path='/use-cases' element={<Resources/>}/> */}
-        {/* <Route path='/pricing' element={<Pricing/>}/> */}
+        <Route path='/use-cases' element={<Resources/>}/>
+        <Route path='/pricing' element={<Pricing/>}/>
         {/* <Route path='/support' element={<Company/>}/> */}
         <Route path='/login' element={<Login/>}/>
         {/* <Route path='/signup' element={<SignUp/>}/> */}
