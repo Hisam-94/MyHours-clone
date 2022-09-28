8ed7cd61060e177ce68f2851be6285064164ed17
Kalash Thakur <83134042+KalashThakur@users.noreply.github.com>
2022-09-29 00:41:34 +0530

    video

--- frontend/src/Pages/Work.jsx
+++ frontend/src/Pages/Work.jsx
@@ -10,6 +10,15 @@ const Work = () => {
           <div className={style.textBlock}>Time tracking was a pain. We fixed it and made time tracking worth the effort. My Hours is easy to use and gets the most out of your data with powerful reporting. Teams that use My Hours are more productive, efficient and organized.</div>
           <button className={style.btn}> <strong className={style.text}>Get Started - It's Free</strong> </button>
       </div>
+
+      {/* ****************Video**************** */}
+    <div>
+        <div className={style.cont2}>
+            <div className={style.cont2Div}>
+                <iframe src="https://www.youtube.com/embed/VpMysclixyQ?rel=0&amp;controls=1&amp;autoplay=0&amp;mute=0&amp;start=0" frameborder="0" style="position:absolute;left:0;top:0;width:100%;height:100%;pointer-events:auto" allow="autoplay; encrypted-media" allowfullscreen="" title="My Hours Intro video"></iframe>
+            </div>
+        </div>
+    </div>
     </div>
   )
 }
