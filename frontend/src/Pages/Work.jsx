b481cb4bd8292b644c9d17b531c04254a3d9567d
Kalash Thakur <83134042+KalashThakur@users.noreply.github.com>
2022-09-29 00:03:03 +0530

    top-section

--- frontend/src/Pages/Work.jsx
+++ frontend/src/Pages/Work.jsx
@@ -5,9 +5,9 @@ const Work = () => {
   return (
     <div>
       <div className={style.topDiv}>
-          <h1>How it works</h1>
-          <h2>& why you should switch to My Hours</h2>
-          <div>Time tracking was a pain. We fixed it and made time tracking worth the effort. My Hours is easy to use and gets the most out of your data with powerful reporting. Teams that use My Hours are more productive, efficient and organized.</div>
+          <h1 className={style.heading45}>How it works</h1>
+          <h2 className={style.heading47}>& why you should switch to My Hours</h2>
+          <div className={style.textBlock}>Time tracking was a pain. We fixed it and made time tracking worth the effort. My Hours is easy to use and gets the most out of your data with powerful reporting. Teams that use My Hours are more productive, efficient and organized.</div>
           <button className={style.btn}> <strong className={style.text}>Get Started - It's Free</strong> </button>
       </div>
     </div>
