e913f83b23229da0bb70f3680b472808284ad323
ellite07 <shelketushar100@gmail.com>
2022-09-29 23:06:25 +0530

    add task form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar'
+import AddTeam from './Pages/Team/AddTeam';
 import AllRoutes from './Routes/AllRoutes'
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <AllRoutes/>     
+      {/* <Navbar/> */}
+      {/* <AllRoutes/>      */}
+
+      {/* branch day03 */}
+      <h1>Team page</h1>
+      <AddTeam/>
     </div>
   );
 }
