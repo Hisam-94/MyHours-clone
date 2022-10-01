41aba4d96c14bc75cbe7b5fedd2883c7aeecade8
ellite07 <shelketushar100@gmail.com>
2022-10-01 12:04:31 +0530

    day4

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Editform from '../Pages/EditUser/Editform'
+import Sidebar from '../Pages/Application/Sidebar/Sidebar'
 import Home from '../Pages/Home'
 import Login from '../Pages/Login'
+import SignUp from '../Pages/SignUp'
 import Pricing from '../Pages/Pricepage/Pricing'
+import { AddProject } from '../Pages/Projects/AddProject'
+import Projects from '../Pages/Projects/Projects'
 import Resources from '../Pages/Resources/Resources'
 import Support from '../Pages/Support/Support'
+import Work from '../Pages/Work'
+import Editform from '../Pages/EditUser/Editform'
 import Teamform from '../Pages/TeamFromValid/Teamform'
 import Teamuser from '../Pages/TeamUser/Teamuser'
-import Work from '../Pages/Work'
-
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<Home/>}/>
-        {/* <Route path='/how-it-works' element={<Product/>}/> */}
         <Route path='/use-cases' element={<Resources/>}/>
         <Route path='/pricing' element={<Pricing/>}/>
         <Route path='/how-it-works' element={<Work/>}/>
-        {/* <Route path='/support' element={<Support/>}/> */}
+        <Route path='/support' element={<Support/>}/>
         <Route path='/login' element={<Login/>}/>
-        {/* <Route path='/signup' element={<SignUp/>}/> */}
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/addprojects' element={<AddProject/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/sidebar' element={<Sidebar/>}/>
         <Route path='/teamform' element={<Teamform/>}/>
         <Route path='/edit/:id' element={<Editform />}/>
         <Route path='/teamuser' element={<Teamuser/>}/>
@@ -28,4 +33,4 @@ const AllRoutes = () => {
   )
 }
 
-export default AllRoutes;
+export default AllRoutes
