b9acbad88f42957eb4c3092324d190bb2a4ec460
Rabi3421 <rabinarayan3421@gmail.com>
2022-10-02 09:15:24 +0530

    unique done

--- backend/Models/user.module.js
+++ backend/Models/user.module.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     full_name: {type:String, required:true},
-    email : {type:String, required:true},
+    email : {type:String, required:true,unique: true},
     password : {type:String, required:true},
     
 })
